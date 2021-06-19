c8e493665e940de8c00ac515c93c82e2154cd736
tanmoy <pdt.tanmoy@gmail.com>
2021-06-19 12:41:26 +0600

    Test of Product find and add has been checked

--- src/main/java/com/tanmoy/inventory/repository/ProductRepo.java
+++ src/main/java/com/tanmoy/inventory/repository/ProductRepo.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tanmoy.inventory.domain.Product;
 
 public interface ProductRepo extends JpaRepository<Product, Integer>{
-
+	
+	Product findById(int id);
 }
--- src/test/java/com/tanmoy/inventory/InventoryApplicationTests.java
+++ src/test/java/com/tanmoy/inventory/InventoryApplicationTests.java
@@ -5,13 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.tanmoy.inventory.domain.Customer;
+import com.tanmoy.inventory.domain.Product;
 import com.tanmoy.inventory.repository.CustomerRepo;
+import com.tanmoy.inventory.repository.ProductRepo;
 
 @SpringBootTest
 class InventoryApplicationTests {
 
 	
 	@Autowired private CustomerRepo custRepo;
+	@Autowired private ProductRepo proRepo;
 	
 	@Test
 	void contextLoads() {
@@ -26,14 +29,29 @@ class InventoryApplicationTests {
 	void findCustomerByUserName() {
 		Customer ct = custRepo.findByUserName("apu");
 		System.out.println(ct.toString());
-	}
+	}	
 	
-	
-	
-	public Customer getCustomer() {
+	private Customer getCustomer() {
 		Customer ct = new Customer("Apu", "Naztech", 1725768409, 0, 
 				"apu.sangma@gmail.com", "Kalachadpur, Dhaka", 0, 0, "apu", "1234");		
 		return ct;
 	}
+	
+	@Test
+	void addProductTest() {
+		proRepo.save(getProduct());
+	}
+	
+	@Test
+	void findProductById() {
+		Product pd = proRepo.findById(8);
+		System.out.println(pd.toString());
+	}
+	
+	private Product getProduct() {
+		return new Product("Sony Xperia XA", 1, "Specification\nRAM 2GB and ROM 16GB Version");
+	}
+	
+	
 
 }
