29f87e38b8e994578522736d93e9784d5936bb9d
tanmoy <pdt.tanmoy@gmail.com>
2021-06-19 12:18:21 +0600

    ProductTypeRepo has been created

---
+++ src/main/java/com/tanmoy/inventory/repository/ProductTypeRepo.java
@@ -0,0 +1,9 @@
+package com.tanmoy.inventory.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tanmoy.inventory.domain.ProductType;
+
+public interface ProductTypeRepo extends JpaRepository<ProductType, Integer>{
+
+}
