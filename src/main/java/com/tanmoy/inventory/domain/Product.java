213d60b0832c357632723ba4f39cfa53ce03e143
tanmoy <pdt.tanmoy@gmail.com>
2021-08-01 19:02:00 +0600

    Product domain validation added

--- src/main/java/com/tanmoy/inventory/domain/Product.java
+++ src/main/java/com/tanmoy/inventory/domain/Product.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Table
 @Entity
@@ -17,6 +19,8 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String productName;
+	@NotBlank(message="Product name is mandatory")
+	@NotNull
 	private int productType;
 	private String details;
 	private double purchaseValue;
