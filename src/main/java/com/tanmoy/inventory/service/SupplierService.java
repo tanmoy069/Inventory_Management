42dac3a9b1b8bbf031c4a543a67d415c405c8644
tanmoy <pdt.tanmoy@gmail.com>
2021-08-04 18:24:12 +0600

    Created SupplierService class

---
+++ src/main/java/com/tanmoy/inventory/service/SupplierService.java
@@ -0,0 +1,42 @@
+package com.tanmoy.inventory.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tanmoy.inventory.domain.Supplier;
+
+@Service
+public class SupplierService extends AbstractService<Supplier>{
+
+	@Override
+	public Supplier findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Supplier> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(Supplier obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Supplier obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
