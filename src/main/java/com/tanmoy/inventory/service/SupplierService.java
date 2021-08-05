a750140f2bb453f3f1fa9429638cd93eb9651ed2
tanmoy <pdt.tanmoy@gmail.com>
2021-08-05 13:38:01 +0600

    TODO has been updated of SupplierService

--- src/main/java/com/tanmoy/inventory/service/SupplierService.java
+++ src/main/java/com/tanmoy/inventory/service/SupplierService.java
@@ -2,41 +2,67 @@ package com.tanmoy.inventory.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tanmoy.inventory.domain.Supplier;
+import com.tanmoy.inventory.repository.SupplierRepo;
 
 @Service
-public class SupplierService extends AbstractService<Supplier>{
+public class SupplierService extends AbstractService<Supplier> {
+
+	private SupplierRepo suppRepo;
+
+	@Autowired
+	public SupplierService(SupplierRepo suppRepo) {
+		this.suppRepo = suppRepo;
+	}
 
 	@Override
 	public Supplier findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return suppRepo.findById(id);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
 	public List<Supplier> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return suppRepo.findAll();
 	}
 
 	@Override
 	public boolean save(Supplier obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			if (findById(obj.getId()) == null) {
+				suppRepo.save(obj);
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean update(Supplier obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			suppRepo.save(obj);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean deleteById(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			suppRepo.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 }
