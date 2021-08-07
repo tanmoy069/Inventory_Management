1012c3560d7b56d2928de330629add22b071e8ea
tanmoy <pdt.tanmoy@gmail.com>
2021-08-07 18:34:06 +0600

    All todo has been implemented of TransactionService class

--- src/main/java/com/tanmoy/inventory/service/TransactionsService.java
+++ src/main/java/com/tanmoy/inventory/service/TransactionsService.java
@@ -2,41 +2,67 @@ package com.tanmoy.inventory.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tanmoy.inventory.domain.Transactions;
+import com.tanmoy.inventory.repository.TransactionsRepo;
 
 @Service
-public class TransactionsService extends AbstractService<Transactions>{
+public class TransactionsService extends AbstractService<Transactions> {
+
+	private TransactionsRepo transRepo;
+
+	@Autowired
+	public TransactionsService(TransactionsRepo transRepo) {
+		this.transRepo = transRepo;
+	}
 
 	@Override
 	public Transactions findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return transRepo.findById(id);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	@Override
 	public List<Transactions> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return transRepo.findAll();
 	}
 
 	@Override
 	public boolean save(Transactions obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			if (findById(obj.getId()) == null) {
+				transRepo.save(obj);
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean update(Transactions obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			transRepo.save(obj);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean deleteById(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			transRepo.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 }
