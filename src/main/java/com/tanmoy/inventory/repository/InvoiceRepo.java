e3cdf8f01b1c488c5305f653cb06c4581d1af030
tanmoy <pdt.tanmoy@gmail.com>
2021-08-09 22:16:29 +0600

    Invoice domain repository InvoiceRepo has been created

---
+++ src/main/java/com/tanmoy/inventory/repository/InvoiceRepo.java
@@ -0,0 +1,11 @@
+package com.tanmoy.inventory.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tanmoy.inventory.domain.Invoice;
+
+public interface InvoiceRepo extends JpaRepository<Invoice, Integer>{
+	
+	Invoice findById(int id);
+
+}
