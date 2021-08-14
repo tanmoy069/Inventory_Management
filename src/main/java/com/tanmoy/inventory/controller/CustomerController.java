9bbd7aee5f1898b8fafe766aa7e3e8feb21ca992
tanmoy <pdt.tanmoy@gmail.com>
2021-08-15 00:22:53 +0600

    CustomerService has been injected to customerController

--- src/main/java/com/tanmoy/inventory/controller/CustomerController.java
+++ src/main/java/com/tanmoy/inventory/controller/CustomerController.java
@@ -1,10 +1,20 @@
 package com.tanmoy.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.service.CustomerService;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	private CustomerService custService;
+	
+	@Autowired
+	public CustomerController(CustomerService custService) {
+		this.custService = custService;
+	}
 
 }
