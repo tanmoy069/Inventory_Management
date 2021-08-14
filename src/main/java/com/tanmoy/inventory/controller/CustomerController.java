fbba5eb1a51e954357aef9bf0d19d3bcb08c636c
tanmoy <pdt.tanmoy@gmail.com>
2021-08-15 00:24:52 +0600

    customer/findall api has been created to get all customer

--- src/main/java/com/tanmoy/inventory/controller/CustomerController.java
+++ src/main/java/com/tanmoy/inventory/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package com.tanmoy.inventory.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tanmoy.inventory.domain.Customer;
 import com.tanmoy.inventory.service.CustomerService;
 
 @RestController
@@ -15,6 +19,11 @@ public class CustomerController {
 	@Autowired
 	public CustomerController(CustomerService custService) {
 		this.custService = custService;
+	}
+	
+	@GetMapping("/findall")
+	public List<Customer> getAllCustomer() {
+		return custService.findAll();
 	}
 
 }
