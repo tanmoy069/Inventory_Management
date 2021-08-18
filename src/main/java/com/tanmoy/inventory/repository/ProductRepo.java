30f5081822c43edc8c18ffb76a4d0065565ab09d
tanmoy <pdt.tanmoy@gmail.com>
2021-08-18 23:42:42 +0600

    findByProductType() method has been added to ProductRepo

--- src/main/java/com/tanmoy/inventory/repository/ProductRepo.java
+++ src/main/java/com/tanmoy/inventory/repository/ProductRepo.java
@@ -1,5 +1,7 @@
 package com.tanmoy.inventory.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tanmoy.inventory.domain.Product;
@@ -7,4 +9,5 @@ import com.tanmoy.inventory.domain.Product;
 public interface ProductRepo extends JpaRepository<Product, Integer>{
 	
 	Product findById(int id);
+	List<Product> findByProductType(int type);
 }
