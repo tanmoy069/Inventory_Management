3bcde65ae60d414e307d62572f03ffd388b9f6f6
tanmoy <pdt.tanmoy@gmail.com>
2021-08-18 23:38:55 +0600

    /product/list get api has been added to ProductController

--- src/main/java/com/tanmoy/inventory/controller/ProductController.java
+++ src/main/java/com/tanmoy/inventory/controller/ProductController.java
@@ -1,5 +1,28 @@
 package com.tanmoy.inventory.controller;
 
-public class ProductController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.domain.Product;
+import com.tanmoy.inventory.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	
+	private ProductService productService;
+
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping("/list")
+	public List<Product> getProductList() {
+		return productService.findAll();
+	}
 }
