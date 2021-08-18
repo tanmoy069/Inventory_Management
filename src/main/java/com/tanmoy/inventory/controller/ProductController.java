1a72923b7f1b5e1625d0dbae4aea51e4d2193566
tanmoy <pdt.tanmoy@gmail.com>
2021-08-18 23:42:56 +0600

    findByProductType() method has been added to ProductController

--- src/main/java/com/tanmoy/inventory/controller/ProductController.java
+++ src/main/java/com/tanmoy/inventory/controller/ProductController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tanmoy.inventory.domain.Product;
+import com.tanmoy.inventory.domain.UserInfo;
 import com.tanmoy.inventory.service.ProductService;
 
 @RestController
@@ -24,5 +26,15 @@ public class ProductController {
 	@GetMapping("/list")
 	public List<Product> getProductList() {
 		return productService.findAll();
+	}
+	
+	@GetMapping("/findby")
+	public UserInfo getUserByParam(@RequestParam(name="username", required = false) String userName,
+			@RequestParam(name="userid", required = false) String userId,
+			@RequestParam(name="email", required = false) String email) {
+		if(userName == null && userId == null && email == null) return new UserInfo();
+		if(userId != null) return userInfoService.findById(userInfoService.getInt(userId));
+		if(email != null) return userInfoService.findByEmail(email);
+		return userInfoService.findByUserName(userName);
 	}
 }
