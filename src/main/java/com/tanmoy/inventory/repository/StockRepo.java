e15d0a4efe26b72d677ef3c095627482aed2aa35
tanmoy <pdt.tanmoy@gmail.com>
2021-08-20 14:10:23 +0600

    Again changed the return type of findByProductId()

--- src/main/java/com/tanmoy/inventory/repository/StockRepo.java
+++ src/main/java/com/tanmoy/inventory/repository/StockRepo.java
@@ -1,7 +1,5 @@
 package com.tanmoy.inventory.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tanmoy.inventory.domain.Stock;
@@ -9,6 +7,6 @@ import com.tanmoy.inventory.domain.Stock;
 public interface StockRepo extends JpaRepository<Stock, Integer>{
 	
 	Stock findById(int id);
-	List<Stock> findByProductId(int productId);
+	Stock findByProductId(int productId);
 
 }
