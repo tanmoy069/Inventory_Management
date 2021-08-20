01c3803985da890f280fdadc005175cf31561ab5
tanmoy <pdt.tanmoy@gmail.com>
2021-08-20 13:45:27 +0600

    /product-type/findall api has been added to ProductTypeController

--- src/main/java/com/tanmoy/inventory/controller/ProductTypeController.java
+++ src/main/java/com/tanmoy/inventory/controller/ProductTypeController.java
@@ -1,5 +1,31 @@
 package com.tanmoy.inventory.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.domain.ProductType;
+import com.tanmoy.inventory.service.ProductTypeService;
+
+@RestController
+@RequestMapping("/product-type")
 public class ProductTypeController {
+	
+	private ProductTypeService proTypeService;
+	
+	@Autowired
+	public ProductTypeController(ProductTypeService proTypeService) {
+		this.proTypeService = proTypeService;
+	}
+	
+	@GetMapping("/findall")
+	public List<ProductType> getProductTypeList(){
+		return proTypeService.findAll();
+	}
+	
+	
 
 }
