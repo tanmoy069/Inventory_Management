ae119ff0cf9d2ccdea97d9ea8bb3bb8b865e1bcc
tanmoy <pdt.tanmoy@gmail.com>
2021-08-20 13:48:11 +0600

    /product-type/findby?id='' api has been added to ProductTypeController

--- src/main/java/com/tanmoy/inventory/controller/ProductTypeController.java
+++ src/main/java/com/tanmoy/inventory/controller/ProductTypeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tanmoy.inventory.domain.ProductType;
@@ -13,19 +14,22 @@ import com.tanmoy.inventory.service.ProductTypeService;
 @RestController
 @RequestMapping("/product-type")
 public class ProductTypeController {
-	
+
 	private ProductTypeService proTypeService;
-	
+
 	@Autowired
 	public ProductTypeController(ProductTypeService proTypeService) {
 		this.proTypeService = proTypeService;
 	}
-	
+
 	@GetMapping("/findall")
-	public List<ProductType> getProductTypeList(){
+	public List<ProductType> getProductTypeList() {
 		return proTypeService.findAll();
 	}
-	
-	
+
+	@GetMapping("/findby")
+	public ProductType getProductById(@RequestParam(name = "id", required = false) String id) {
+		return proTypeService.findById(proTypeService.getInt(id));
+	}
 
 }
