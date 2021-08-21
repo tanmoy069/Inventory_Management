b9120cf24a92f6a39fe5ed19110401ae22d76c94
tanmoy <pdt.tanmoy@gmail.com>
2021-08-21 22:27:52 +0600

    StockService injected to StockControler constructor

--- src/main/java/com/tanmoy/inventory/controller/StockController.java
+++ src/main/java/com/tanmoy/inventory/controller/StockController.java
@@ -1,5 +1,20 @@
 package com.tanmoy.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.service.StockService;
+
+@RestController
+@RequestMapping("/stock")
 public class StockController {
+	
+	private StockService stockService;
+	
+	@Autowired
+	public StockController(StockService stockService) {
+		this.stockService = stockService;
+	}
 
 }
