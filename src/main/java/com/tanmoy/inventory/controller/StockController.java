0798e8c09c3092a2ca94fb9a06101af92544469c
tanmoy <pdt.tanmoy@gmail.com>
2021-08-21 22:29:54 +0600

    /stock/findall api has been added to get all stock list

--- src/main/java/com/tanmoy/inventory/controller/StockController.java
+++ src/main/java/com/tanmoy/inventory/controller/StockController.java
@@ -1,9 +1,13 @@
 package com.tanmoy.inventory.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tanmoy.inventory.domain.Stock;
 import com.tanmoy.inventory.service.StockService;
 
 @RestController
@@ -15,6 +19,11 @@ public class StockController {
 	@Autowired
 	public StockController(StockService stockService) {
 		this.stockService = stockService;
+	}
+	
+	@GetMapping("/findall")
+	public List<Stock> getStockList(){
+		return stockService.findAll(); 
 	}
 
 }
