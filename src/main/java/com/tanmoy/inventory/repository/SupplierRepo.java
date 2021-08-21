774947d44216bb50bc3a19d90fa6bf56c274c1d5
tanmoy <pdt.tanmoy@gmail.com>
2021-08-21 23:17:02 +0600

    findByNameContains() added to SupplierRepo

--- src/main/java/com/tanmoy/inventory/repository/SupplierRepo.java
+++ src/main/java/com/tanmoy/inventory/repository/SupplierRepo.java
@@ -9,7 +9,8 @@ import com.tanmoy.inventory.domain.Supplier;
 public interface SupplierRepo extends JpaRepository<Supplier, Integer>{
 	
 	Supplier findById(int i);
-	List<Supplier> findByCompanyNameContains(String name);
+	List<Supplier> findByCompanyNameContains(String companyName);
+	List<Supplier> findByNameContains(String name);
 	Supplier findByEmail(String email);
 	
 }
