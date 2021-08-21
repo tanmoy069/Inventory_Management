e14831ee21c6fd653f016025001b473b5e122d3f
tanmoy <pdt.tanmoy@gmail.com>
2021-08-21 23:10:51 +0600

    /stock/findby?id=''&productId='' has been added

--- src/main/java/com/tanmoy/inventory/controller/StockController.java
+++ src/main/java/com/tanmoy/inventory/controller/StockController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tanmoy.inventory.domain.Stock;
@@ -24,6 +25,13 @@ public class StockController {
 	@GetMapping("/findall")
 	public List<Stock> getStockList(){
 		return stockService.findAll(); 
+	}
+	
+	@GetMapping("/findby")
+	public Stock getProductById(@RequestParam(name = "id", required = false) String id,
+			@RequestParam(name = "productId", required = false) String productId) {
+		if(id == null) return stockService.findByProductId(stockService.getInt(productId));
+		return stockService.findById(stockService.getInt(id));
 	}
 
 }
