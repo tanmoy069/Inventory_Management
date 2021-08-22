aea25c7e67175647c70cf47907aeda0457e61176
tanmoy <pdt.tanmoy@gmail.com>
2021-08-23 00:03:38 +0600

    /supplier/findall api added to SupplierController to get all supplier

--- src/main/java/com/tanmoy/inventory/controller/SupplierController.java
+++ src/main/java/com/tanmoy/inventory/controller/SupplierController.java
@@ -1,9 +1,13 @@
 package com.tanmoy.inventory.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tanmoy.inventory.domain.Supplier;
 import com.tanmoy.inventory.service.SupplierService;
 
 @RestController
@@ -15,6 +19,11 @@ public class SupplierController {
 	@Autowired
 	public SupplierController(SupplierService suppService) {
 		this.suppService = suppService;
+	}
+	
+	@GetMapping("/findall")
+	public List<Supplier> getSupplierList(){
+		return suppService.findAll();
 	}
 
 }
