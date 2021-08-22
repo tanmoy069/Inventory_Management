d1352d4332d208a4a019d2df8b9dd0c3e9da8702
tanmoy <pdt.tanmoy@gmail.com>
2021-08-22 23:37:51 +0600

    SupplerService dependency injected to SupplierController

--- src/main/java/com/tanmoy/inventory/controller/SupplierController.java
+++ src/main/java/com/tanmoy/inventory/controller/SupplierController.java
@@ -1,5 +1,20 @@
 package com.tanmoy.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.service.SupplierService;
+
+@RestController
+@RequestMapping("/supplier")
 public class SupplierController {
+	
+	private SupplierService suppService;
+	
+	@Autowired
+	public SupplierController(SupplierService suppService) {
+		this.suppService = suppService;
+	}
 
 }
