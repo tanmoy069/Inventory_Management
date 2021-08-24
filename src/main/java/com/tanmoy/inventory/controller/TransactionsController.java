1c8e75b9377a80b9b79571f03978c2e79698fa48
tanmoy <pdt.tanmoy@gmail.com>
2021-08-25 00:01:59 +0600

    /transactions/findby?id='' api has been added to TransactionsController

--- src/main/java/com/tanmoy/inventory/controller/TransactionsController.java
+++ src/main/java/com/tanmoy/inventory/controller/TransactionsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tanmoy.inventory.domain.Transactions;
@@ -24,6 +25,11 @@ public class TransactionsController {
 	@GetMapping("/findall")
 	public List<Transactions> getTransactionsList() {
 		return transService.findAll();
+	}
+	
+	@GetMapping("/findby")
+	public Transactions findTransactionsById(@RequestParam(name="id", required = true) String id) {
+		return transService.findById(transService.getInt(id));
 	}
 	
 	
