6ac802c10d59936f5ca10701122d603e73f15d2a
tanmoy <pdt.tanmoy@gmail.com>
2021-08-24 22:32:10 +0600

    TransactionsService dependency injected to TransactionsController

--- src/main/java/com/tanmoy/inventory/controller/TransactionsController.java
+++ src/main/java/com/tanmoy/inventory/controller/TransactionsController.java
@@ -1,5 +1,19 @@
 package com.tanmoy.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.service.TransactionsService;
+
+@RestController
+@RequestMapping("/transactions")
 public class TransactionsController {
 
+	private TransactionsService transService;
+
+	@Autowired
+	public TransactionsController(TransactionsService transService) {
+		this.transService = transService;
+	}
 }
