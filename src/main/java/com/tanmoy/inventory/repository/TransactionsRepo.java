f5d370fc40024fdeab3b2a04111aad7d70b77b91
tanmoy <pdt.tanmoy@gmail.com>
2021-09-01 00:02:49 +0600

    Change the type of findByTransDate params from Date to String

--- src/main/java/com/tanmoy/inventory/repository/TransactionsRepo.java
+++ src/main/java/com/tanmoy/inventory/repository/TransactionsRepo.java
@@ -1,6 +1,5 @@
 package com.tanmoy.inventory.repository;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +9,7 @@ import com.tanmoy.inventory.domain.Transactions;
 public interface TransactionsRepo extends JpaRepository<Transactions, Integer>{
 	
 	Transactions findById(int id);
-	List<Transactions> findByTransDate(Date tDate);
+	List<Transactions> findByTransDate(String tDate);
 	List<Transactions> findByUserId(int userId);
 	List<Transactions> findByProductId(int productId);
 	List<Transactions> findByCustomerId(int customerId);
