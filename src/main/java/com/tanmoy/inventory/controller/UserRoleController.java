2da68313e902a846ec9c2b5be06a0e857d8e2f0c
tanmoy <pdt.tanmoy@gmail.com>
2021-09-02 23:25:16 +0600

    /user-role/findall api has been added to get all user role list

--- src/main/java/com/tanmoy/inventory/controller/UserRoleController.java
+++ src/main/java/com/tanmoy/inventory/controller/UserRoleController.java
@@ -1,5 +1,7 @@
 package com.tanmoy.inventory.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,20 @@ import com.tanmoy.inventory.service.UserRoleService;
 public class UserRoleController {
 
 	private UserRoleService roleService;
-	
+
 	@Autowired
 	public UserRoleController(UserRoleService roleService) {
 		this.roleService = roleService;
 	}
-	
+
 	@GetMapping("/findby")
-	public UserRole findTransactionsById(@RequestParam(name="id", required = true) String id) {
+	public UserRole findUserRoleById(@RequestParam(name = "id", required = true) String id) {
 		return roleService.findById(roleService.getInt(id));
-	}	
-	
+	}
+
+	@GetMapping("/findall")
+	public List<UserRole> getUserRoleList() {
+		return roleService.findAll();
+	}
+
 }
