af42d928a40d140899f576a809fa1011b94f0958
tanmoy <pdt.tanmoy@gmail.com>
2021-09-02 23:23:15 +0600

    /user-role/findby?id='' api has been created to get UserRole by roleid

--- src/main/java/com/tanmoy/inventory/controller/UserRoleController.java
+++ src/main/java/com/tanmoy/inventory/controller/UserRoleController.java
@@ -1,9 +1,12 @@
 package com.tanmoy.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tanmoy.inventory.domain.UserRole;
 import com.tanmoy.inventory.service.UserRoleService;
 
 @RestController
@@ -16,4 +19,10 @@ public class UserRoleController {
 	public UserRoleController(UserRoleService roleService) {
 		this.roleService = roleService;
 	}
+	
+	@GetMapping("/findby")
+	public UserRole findTransactionsById(@RequestParam(name="id", required = true) String id) {
+		return roleService.findById(roleService.getInt(id));
+	}	
+	
 }
