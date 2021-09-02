49de9af2931085e2c0f582f8a2e5913c07c9f7db
tanmoy <pdt.tanmoy@gmail.com>
2021-09-02 23:20:18 +0600

    UserRoleService has been injected to UserRoleController

--- src/main/java/com/tanmoy/inventory/controller/UserRoleController.java
+++ src/main/java/com/tanmoy/inventory/controller/UserRoleController.java
@@ -1,5 +1,19 @@
 package com.tanmoy.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tanmoy.inventory.service.UserRoleService;
+
+@RestController
+@RequestMapping("/user-role")
 public class UserRoleController {
 
+	private UserRoleService roleService;
+	
+	@Autowired
+	public UserRoleController(UserRoleService roleService) {
+		this.roleService = roleService;
+	}
 }
