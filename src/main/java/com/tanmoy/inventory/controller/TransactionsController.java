c4e55880090024abd4e31f6bc2f1d6b5e7e74671
tanmoy <pdt.tanmoy@gmail.com>
2021-10-15 00:19:32 +0600

    @RequestBody added in save method to support JSON data also

--- src/main/java/com/tanmoy/inventory/controller/TransactionsController.java
+++ src/main/java/com/tanmoy/inventory/controller/TransactionsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,7 +59,7 @@ public class TransactionsController {
 	}
 	
 	@PostMapping("/save")
-	public boolean saveTransactions(Transactions transactions) {
+	public boolean saveTransactions(@RequestBody Transactions transactions) {
 		return transService.save(transactions);
 	}
 	
