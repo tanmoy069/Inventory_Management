3d5152e4f83b22755ea6e27ab7eeeb83ee222105
tanmoy <pdt.tanmoy@gmail.com>
2021-10-16 16:24:21 +0600

    Added @RequestBody saveSupplier method

--- src/main/java/com/tanmoy/inventory/controller/SupplierController.java
+++ src/main/java/com/tanmoy/inventory/controller/SupplierController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,7 +43,7 @@ public class SupplierController {
 	}
 	
 	@PostMapping("/save")
-	public boolean saveSupplier(Supplier supplier) {
+	public boolean saveSupplier(@RequestBody Supplier supplier) {
 		return suppService.save(supplier);
 	}
 	
