266542c905c7fa3f43987f1e94fd01b5d5a0dd72
tanmoy <pdt.tanmoy@gmail.com>
2021-10-22 00:05:32 +0600

    /product/save api has been added to save product

--- src/main/java/com/tanmoy/inventory/controller/ProductController.java
+++ src/main/java/com/tanmoy/inventory/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,11 @@ public class ProductController {
 	public Product getProduct(@RequestParam(name = "id", required = false) String id) {
 		if (id == null) return new Product();
 		return productService.findById(productService.getInt(id));
+	}
+	
+	@GetMapping("/save")
+	public boolean saveProduct(@RequestBody Product product) {
+		return productService.save(product);
 	}
 
 }
