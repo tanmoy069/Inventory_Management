df67ce2c387e3794fab78536849a9b514cd12d6d
tanmoy <pdt.tanmoy@gmail.com>
2021-10-22 00:06:14 +0600

    Change the mapping type from get to post of saveProduct()

--- src/main/java/com/tanmoy/inventory/controller/ProductController.java
+++ src/main/java/com/tanmoy/inventory/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,9 +36,11 @@ public class ProductController {
 		return productService.findById(productService.getInt(id));
 	}
 	
-	@GetMapping("/save")
+	@PostMapping("/save")
 	public boolean saveProduct(@RequestBody Product product) {
 		return productService.save(product);
 	}
+	
+	
 
 }
